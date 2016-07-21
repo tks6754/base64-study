7012389cfa93e6e687b441a960b8ceb26cd11073
MiaoKun <tks6754@gmail.com>
2016-07-21 10:16:08 +0800

    pom
    pom整理，注意guava的base64工具在guava的io工具中

---
+++ src/main/java/com/sun/misc/demo/MiscBase64Demo.java
@@ -0,0 +1,17 @@
+package com.sun.misc.demo;
+
+import com.google.common.io.BaseEncoding;
+
+/**
+ * Created by miaokun on 2016/7/21.
+ */
+public class MiscBase64Demo {
+    public static void main(String[] args) {
+
+    }
+
+
+    static String encode(){}
+    BaseEncoding
+
+}
